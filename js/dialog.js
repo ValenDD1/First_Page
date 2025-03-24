5824c4ac453cceb48bfde94864f957997c2fff68
valentino cuenca escobar <cvpaginaeb@gmail.com>
2025-03-24 03:53:48 -0500

    carpeta interactivo creado y hecho

---
+++ js/dialog.js
@@ -0,0 +1,7 @@
+const showButton = document.querySelector("#show-button");
+showButton.addEventListener("click", function () {
+    const alertDialog = document.querySelector("#alert-dialog2");
+    alertDialog.show();
+});
+
+
